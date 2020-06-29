021600e9138bc0aafef2bc8be148b1c039a549bc
Majestik <vbifjcby1998@yandex.ru>
2020-06-29 21:20:47 +0300

    searching for a cat

--- mv/misf.py
+++ mv/misf.py
@@ -1,1 +1,26 @@
-# TODO: murrr
+import time
+
+my_pets = ['lion', 'dog', 'skunk', 'hamster', 'cat', 'monkey']
+i = -1
+while i < len(my_pets):
+    i += 1
+    if i == 2:
+        print('Скунса даже проверять не будем, бее!')
+        time.sleep(2)
+        continue
+    pet = my_pets[i]
+    print('Проверяем ', pet)
+    time.sleep(1)
+    print('3')
+    time.sleep(1)
+    print('2')
+    time.sleep(1)
+    print('1')
+    time.sleep(1)
+    if pet == 'cat':
+        print('Ура, кот найден!')
+        break
+    else:
+        print('Это не кот!!!')
+    time.sleep(2)
+print('дотвиданя!')
