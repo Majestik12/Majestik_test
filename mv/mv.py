cf788d5fe6631a82b05a422fd58d5a3da8ebb14a
Majestik <vbifjcby1998@yandex.ru>
2020-07-02 16:57:35 +0300

    user_input

--- mv/mv.py
+++ mv/mv.py
@@ -1,2 +1,8 @@
-print ('Hello World')
-# TODO: dfsdgghfdh
+while True:
+    user_input = input('Введите 42 >> ')
+    result = int(user_input)
+    if result == 42:
+        print('Спасибо за сотрудничество!')
+        break
+    else:
+        print('Я просил 42, а Вы ввели', result, 'Попробуйте еще раз...')
