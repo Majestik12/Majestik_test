172f9c41b110c762a1d46ed88bafafb3502f8661
Majestik <vbifjcby1998@yandex.ru>
2020-07-10 19:48:18 +0300

    i did dz3\01_deys_in_month

--- dz3/01_days_in_month.py
+++ dz3/01_days_in_month.py
@@ -10,5 +10,32 @@
 user_input = input("Введите, пожалуйста, номер месяца: ")
 month = int(user_input)
 print('Вы ввели', month)
+month_1 = 0
+if month == 1:
+    month_1 = 31
+elif month == 2:
+    month_1 = 28
+elif month == 3:
+    month_1 = 31
+elif month == 4:
+    month_1 = 30
+elif month == 5:
+    month_1 = 31
+elif month == 6:
+    month_1 = 30
+elif month == 7:
+    month_1 = 31
+elif month == 8:
+    month_1 = 31
+elif month == 9:
+    month_1 = 30
+elif month == 10:
+    month_1 = 31
+elif month == 11:
+    month_1 = 30
+elif month == 12:
+    month_1 = 31
+else:
 
-# TODO здесь ваш код
+    print("Вы ввели некорректное число")
+print('Дней в этом месяце %s' % month_1)
