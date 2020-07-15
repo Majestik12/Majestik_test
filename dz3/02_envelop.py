7178a2dd28251977742ad45db4b1086eb7f2cb2a
Majestik <vbifjcby1998@yandex.ru>
2020-07-15 19:36:04 +0300

    поправил dz3\02

--- dz3/02_envelop.py
+++ dz3/02_envelop.py
@@ -35,7 +35,7 @@ else:
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
 hole_x, hole_y = 8, 9
-# brick_x, brick_y, brick_z = 11, 10, 2
+brick_x, brick_y, brick_z = 11, 10, 2
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
 # brick_x, brick_y, brick_z = 10, 2, 11
