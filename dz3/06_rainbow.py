7a418dffb4a02087020069608edfc2fedecacd72
Majestik <vbifjcby1998@yandex.ru>
2020-07-17 19:37:45 +0300

    i po4ti did dz3\06_rainbow

--- dz3/06_rainbow.py
+++ dz3/06_rainbow.py
@@ -4,8 +4,20 @@
 
 import simple_draw as sd
 
+sd.resolution = (1200, 600)
+
 rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
                   sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
+x1 = 50
+x2 = 350
+y1 = 50
+y2 = 450
+for color in rainbow_colors:
+    point = sd.get_point(x1, y1)
+    point_2 = sd.get_point(x2, y2)
+    sd.line(point, point_2, color, 4)
+    x1 += 5
+    x2 += 5
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
 # TODO здесь ваш код
