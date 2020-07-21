751794d432ed1fd6fe27d0e1a9c72ccdb28c8f1b
Majestik <vbifjcby1998@yandex.ru>
2020-07-21 17:42:44 +0300

    i did dz3\07_wall

--- dz3/07_wall.py
+++ dz3/07_wall.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
+sd.resolution = (1000, 500)
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
 # TODO здесь ваш код
 
-simple_draw.pause()
+step = 50
+for y in range(0, 1000, 50):
+    y1 = y + 50
+    step -= 50
+    for x in range(step, 1000, 100):
+        x1 = x + 100
+        point = sd.get_point(x, y)
+        point1 = sd.get_point(x1, y1)
+        sd.rectangle(point, point1, color=sd.COLOR_ORANGE, width=1)
+sd.pause()
